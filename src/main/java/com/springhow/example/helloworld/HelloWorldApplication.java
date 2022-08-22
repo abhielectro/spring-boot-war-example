8a9423b0ae8b157179c0b89daf0ab95281e6dcd8
abhielectro <abhinandan.it@gmail.com>
2022-08-22 18:44:57 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "App is build by Abhinandan";
+        return "App1 is build by Abhinandan";
     }
 
 }
